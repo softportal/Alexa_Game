446bd22bd3ee37ae393e6ad5083122abd7ffb380
Sergio Semedi <sergio.semedi@gmail.com>
2019-09-21 14:08:49 +0200

    ahorcado features

--- src/lib/ahorcado_app.js
+++ src/lib/ahorcado_app.js
@@ -1,13 +1,19 @@
 'use strict';
 
+const NULL = '-';
+var pry = require('pryjs');
+
 function Happ (opts) {
     this.data = require('../../bd/ahorcado.json');
-
 }
 
+//use only testing purposes
 Happ.prototype.test = function () {
-    var res = this.search_by_category_level("ciudades", 0);
+    var word = this.search_by_category_level("ciudades", 0);
 
+    console.log(word);
+
+    var res = this.solve(word, [NULL, NULL, NULL, NULL, NULL, NULL], "a");
     console.log(res);
 };
 
@@ -16,12 +22,50 @@ Happ.prototype.search_by_category_level = function (category, level) {
     return this.data["category"][category]["words"][level];
 };
 
+Happ.prototype.solve = function (word, state, letter) {
+    var count = 0;
+    var split = word["split"]
+    if (state.length != split.length){
+        return response_error("state not matching word!");
+    }
+
+
+    split.forEach(function (v, i) {
+        if (state[i] == NULL){
+            if (letter == split[i]){
+                count++;
+                state[i] = letter
+            }
+        }
+    });
+
+    return response(state, count);
+};
+
 
 /*********************************************************
  * Private function                                      *
  *********************************************************/
 function private_test(chain) {
     console.log(chain);
+}
+
+function response(state, count){
+    var render = function(){
+        return state.join('  ');
+    }
+
+    return {
+        "render": render(),
+        "state": state,
+        "count": count
+    };
+}
+
+function response_error(error_msg) {
+    return {
+        "error": error_msg
+    };
 }
 
 var p = new Happ({});
