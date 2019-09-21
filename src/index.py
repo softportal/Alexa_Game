f7fc3b14b044f5e7747f212973816ed144f016bf
Sergio Semedi <sergio.semedi@gmail.com>
2019-09-21 10:44:14 +0200

    sync

--- src/index.py
+++ src/index.py
@@ -2,67 +2,187 @@
 // Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
 // session persistence, api calls, and more.
 const Alexa = require('ask-sdk-core');
+var persistenceAdapter = getPersistenceAdapter();
+
+const moment = require('moment-timezone'); // will help us do all the birthday math
+
+// i18n dependencies. i18n is the main module, sprintf allows us to include variables with '%s'.
+const i18n = require('i18next');
+const sprintf = require('i18next-sprintf-postprocessor');
+
+// We import language strings object containing all of our strings. 
+// The keys for each string will then be referenced in our code
+// e.g. requestAttributes.t('WELCOME_MSG')
+const languageStrings = require('./localisation');
+
+function getPersistenceAdapter() {
+    // This function is an indirect way to detect if this is part of an Alexa-Hosted skill
+    function isAlexaHosted() {
+        return process.env.S3_PERSISTENCE_BUCKET ? true : false;
+    }
+    const tableName = 'happy_birthday_table';
+    if(isAlexaHosted()) {
+        const {S3PersistenceAdapter} = require('ask-sdk-s3-persistence-adapter');
+        return new S3PersistenceAdapter({ 
+            bucketName: process.env.S3_PERSISTENCE_BUCKET
+        });
+    } else {
+        // IMPORTANT: don't forget to give DynamoDB access to the role you're to run this lambda (IAM)
+        const {DynamoDbPersistenceAdapter} = require('ask-sdk-dynamodb-persistence-adapter');
+        return new DynamoDbPersistenceAdapter({ 
+            tableName: tableName,
+            createTable: true
+        });
+    }
+}
 
 const LaunchRequestHandler = {
-  canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
-  },
-  handle(handlerInput) {
-    const speechText = 'Welcome to the Alexa Skills Kit, you can say hello!';
-
-    return handlerInput.responseBuilder
-      .speak(speechText)
-      .reprompt(speechText)
-      //.cardRenderer("Hello", "Alvi subnormal", "")
-      //.withLinkAccountCard()
-      .withStandardCard('Hello World', "https://www.optoma.es/images/ProductApplicationFeatures/4kuhd/banner.jpg", "", "https://www.optoma.es/images/ProductApplicationFeatures/4kuhd/banner.jpg")
-      .getResponse();
-  }
-};
-const HelloWorldIntentHandler = {
     canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'HelloWorldIntent';
+        return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
     },
     handle(handlerInput) {
-        const speakOutput = 'Hello World!';
+        const {attributesManager} = handlerInput;
+        const requestAttributes = attributesManager.getRequestAttributes();
+        const sessionAttributes = attributesManager.getSessionAttributes();
+        
+        const category = sessionAttributes['category'];
+        
+        let speechText = requestAttributes.t('WELCOME_MSG');
+
+        if(category){
+            speechText = requestAttributes.t('REGISTER_MSG', category)
+        }
+        
         return handlerInput.responseBuilder
-            .speak(speakOutput)
-            .cardRenderer("Hello", "Alvi subnormal")
-            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            .speak(speechText)
+            .reprompt(speechText)
             .getResponse();
     }
 };
+
+const RegisterCategoryIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'RegisterBirthdayIntent';
+    },
+    handle(handlerInput) {
+        const {attributesManager} = handlerInput;
+        const requestAttributes = attributesManager.getRequestAttributes();
+        const sessionAttributes = attributesManager.getSessionAttributes();
+        const {intent} = handlerInput.requestEnvelope.request;
+
+        const day = intent.slots.day.value;
+        const month = intent.slots.month.resolutions.resolutionsPerAuthority[0].values[0].value.id;
+        const monthName = intent.slots.month.resolutions.resolutionsPerAuthority[0].values[0].value.name;
+        const year = intent.slots.year.value;
+        
+        sessionAttributes['day'] = day;
+        sessionAttributes['month'] = month;
+        sessionAttributes['monthName'] = monthName;
+        sessionAttributes['year'] = year;
+
+        return handlerInput.responseBuilder
+            .speak(requestAttributes.t('REGISTER_MSG', day, monthName, year) + requestAttributes.t('HELP_MSG'))
+            .reprompt(requestAttributes.t('HELP_MSG'))
+            .getResponse();
+    }
+};
+
+const SayBirthdayIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'SayBirthdayIntent';
+    },
+    async handle(handlerInput) {
+        const {attributesManager} = handlerInput;
+        const requestAttributes = attributesManager.getRequestAttributes();
+        const sessionAttributes = attributesManager.getSessionAttributes();
+
+        const day = sessionAttributes['day'];
+        const month = sessionAttributes['month'];
+        const year = sessionAttributes['year'];
+        
+        let speechText;
+        if(day && month && year){    
+            const timezone = 'Europe/Madrid'; // we'll change this later to retrieve the timezone from the device
+            const today = moment().tz(timezone).startOf('day');
+            const wasBorn = moment(`${month}/${day}/${year}`, "MM/DD/YYYY").tz(timezone).startOf('day');
+            const nextBirthday = moment(`${month}/${day}/${today.year()}`, "MM/DD/YYYY").tz(timezone).startOf('day');
+            if(today.isAfter(nextBirthday)){
+                nextBirthday.add('years', 1);
+            }
+            const age = today.diff(wasBorn, 'years');
+            const daysLeft = nextBirthday.startOf('day').diff(today, 'days'); // same days returns 0
+            speechText = requestAttributes.t('SAY_MSG', daysLeft, age + 1);
+            if(daysLeft === 0) {
+                speechText = requestAttributes.t('GREET_MSG', age);
+            }
+            speechText += requestAttributes.t('OVERWRITE_MSG');
+        } else {
+            speechText = requestAttributes.t('MISSING_MSG');
+        }
+        
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .reprompt(requestAttributes.t('HELP_MSG'))
+            .getResponse();
+    }
+};
+
 const HelpIntentHandler = {
     canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.HelpIntent';
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
     },
     handle(handlerInput) {
-        const speakOutput = 'You can say hello to me! How can I help?';
+        const {attributesManager} = handlerInput;
+        const requestAttributes = attributesManager.getRequestAttributes();
+        const speechText = requestAttributes.t('HELP_MSG');
 
         return handlerInput.responseBuilder
-            .speak(speakOutput)
-            .reprompt(speakOutput)
+            .speak(speechText)
+            .reprompt(speechText)
             .getResponse();
     }
 };
+
 const CancelAndStopIntentHandler = {
     canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-            && (Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.CancelIntent'
-                || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.StopIntent');
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent'
+                || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
     },
     handle(handlerInput) {
-        const speakOutput = 'Goodbye!';
+        const {attributesManager} = handlerInput;
+        const requestAttributes = attributesManager.getRequestAttributes();
+        const speechText = requestAttributes.t('GOODBYE_MSG');
+
         return handlerInput.responseBuilder
-            .speak(speakOutput)
+            .speak(speechText)
             .getResponse();
     }
 };
+
+const FallbackIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.FallbackIntent';
+    },
+    handle(handlerInput) {
+        const {attributesManager} = handlerInput;
+        const requestAttributes = attributesManager.getRequestAttributes();
+        const speechText = requestAttributes.t('FALLBACK_MSG');
+
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .reprompt(speechText)
+            .getResponse();
+    }
+};
+
 const SessionEndedRequestHandler = {
     canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'SessionEndedRequest';
+        return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
     },
     handle(handlerInput) {
         // Any cleanup logic goes here.
@@ -76,14 +196,16 @@ const SessionEndedRequestHandler = {
 // handler chain below.
 const IntentReflectorHandler = {
     canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest';
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest';
     },
     handle(handlerInput) {
-        const intentName = Alexa.getIntentName(handlerInput.requestEnvelope);
-        const speakOutput = `You just triggered ${intentName}`;
+        const {attributesManager} = handlerInput;
+        const requestAttributes = attributesManager.getRequestAttributes();
+        const intentName = handlerInput.requestEnvelope.request.intent.name;
+        const speechText = requestAttributes.t('REFLECTOR_MSG', intentName);
 
         return handlerInput.responseBuilder
-            .speak(speakOutput)
+            .speak(speechText)
             //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
             .getResponse();
     }
@@ -97,29 +219,90 @@ const ErrorHandler = {
         return true;
     },
     handle(handlerInput, error) {
-        console.log(`~~~~ Error handled: ${error.stack}`);
-        const speakOutput = `Sorry, I had trouble doing what you asked. Please try again.`;
+        const {attributesManager} = handlerInput;
+        const requestAttributes = attributesManager.getRequestAttributes();
+        const speechText = requestAttributes.t('ERROR_MSG');
+
+        console.log(`~~~~ Error handled: ${error.message}`);
 
         return handlerInput.responseBuilder
-            .speak(speakOutput)
-            .reprompt(speakOutput)
+            .speak(speechText)
+            .reprompt(speechText)
             .getResponse();
     }
 };
 
-// The SkillBuilder acts as the entry point for your skill, routing all request and response
-// payloads to the handlers above. Make sure any new handlers or interceptors youve
-// defined are included below. The order matters - theyre processed top to bottom.
+// This request interceptor will log all incoming requests to this lambda
+const LoggingRequestInterceptor = {
+    process(handlerInput) {
+        console.log(`Incoming request: ${JSON.stringify(handlerInput.requestEnvelope.request)}`);
+    }
+};
+
+// This response interceptor will log all outgoing responses of this lambda
+const LoggingResponseInterceptor = {
+    process(handlerInput, response) {
+      console.log(`Outgoing response: ${JSON.stringify(response)}`);
+    }
+};
+
+// This request interceptor will bind a translation function 't' to the requestAttributes.
+const LocalizationRequestInterceptor = {
+  process(handlerInput) {
+    const localizationClient = i18n.use(sprintf).init({
+      lng: handlerInput.requestEnvelope.request.locale,
+      overloadTranslationOptionHandler: sprintf.overloadTranslationOptionHandler,
+      resources: languageStrings,
+      returnObjects: true
+    });
+    const attributes = handlerInput.attributesManager.getRequestAttributes();
+    attributes.t = function (...args) {
+      return localizationClient.t(...args);
+    }
+  }
+};
+
+const LoadAttributesRequestInterceptor = {
+    async process(handlerInput) {
+        if(handlerInput.requestEnvelope.session['new']){ //is this a new session?
+            const {attributesManager} = handlerInput;
+            const persistentAttributes = await attributesManager.getPersistentAttributes() || {};
+            //copy persistent attribute to session attributes
+            handlerInput.attributesManager.setSessionAttributes(persistentAttributes);
+        }
+    }
+};
+
+const SaveAttributesResponseInterceptor = {
+    async process(handlerInput, response) {
+        const {attributesManager} = handlerInput;
+        const sessionAttributes = attributesManager.getSessionAttributes();
+        const shouldEndSession = (typeof response.shouldEndSession === "undefined" ? true : response.shouldEndSession);//is this a session end?
+        if(shouldEndSession || handlerInput.requestEnvelope.request.type === 'SessionEndedRequest') { // skill was stopped or timed out            
+            attributesManager.setPersistentAttributes(sessionAttributes);
+            await attributesManager.savePersistentAttributes();
+        }
+    }
+};
+
+// This handler acts as the entry point for your skill, routing all request and response
+// payloads to the handlers above. Make sure any new handlers or interceptors you've
+// defined are included below. The order matters - they're processed top to bottom.
 exports.handler = Alexa.SkillBuilders.custom()
     .addRequestHandlers(
         LaunchRequestHandler,
-        HelloWorldIntentHandler,
+        RegisterBirthdayIntentHandler,
+        SayBirthdayIntentHandler,
         HelpIntentHandler,
         CancelAndStopIntentHandler,
         SessionEndedRequestHandler,
-        IntentReflectorHandler, // make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
-    )
-    .addErrorHandlers(
-        ErrorHandler,
-    )
-    .lambda();
+        IntentReflectorHandler) // make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
+        .addRequestInterceptors(
+            LocalizationRequestInterceptor,
+            LoggingRequestInterceptor,
+            LoadAttributesRequestInterceptor)
+        .addResponseInterceptors(
+            LoggingResponseInterceptor,
+            SaveAttributesResponseInterceptor)
+        .withPersistenceAdapter(persistenceAdapter)
+        .lambda();
---
+++ src/messages.js
@@ -0,0 +1,17 @@
+module.exports = {
+    es: {
+        translation: {
+            WELCOME_MSG: 'Bienvenido! Escoge una categoría. Las disponibles son, monumentos, famosos o animales ',
+            REGISTER_MSG: 'Se ha registrado la categoría %s. ',
+            SAY_MSG: 'Quedan %s días para que cumplas %s años. ',
+            GREET_MSG: 'Feliz cumpleaños! Hoy cumples %s! ',
+            MISSING_MSG: 'Parece que aun no me has dicho tu fecha de cumpleaños. Prueba decir, registra mi cumpleaños. ',
+            OVERWRITE_MSG: 'Si quieres cambiar la fecha solo di, registra mi cumpleaños',
+            HELP_MSG: 'Puedes preguntarme, cuanto falta para mi cumpleaños? . O registar la fecha diciendo, registra mi cumpleaños. ',
+            GOODBYE_MSG: 'Hasta luego! ',
+            REFLECTOR_MSG: 'Acabas de activar %s ',
+            FALLBACK_MSG: 'Lo siento, no se nada sobre eso. Por favor inténtalo otra vez. ',
+            ERROR_MSG: 'Lo siento, ha habido un problema. Por favor inténtalo otra vez. '
+        }
+    }
+}
